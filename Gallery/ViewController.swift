33473a60043ef5188c9a0f540e2acc7b52f52433
Danila Okunev <danila.okv@gmail.com>
2026-06-05 12:55:43 +0300

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Danila Okunev on 5.06.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
