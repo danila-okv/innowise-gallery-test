2429864925fc277bcabe5954a725c6560dd0104e
Danila Okunev <danila.okv@gmail.com>
2026-06-09 16:14:15 +0300

    [ADDED] Configured unit test target using Swift Testing

---
+++ GalleryTests/GalleryTests.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryTests.swift
+//  GalleryTests
+//
+//  Created by Danila Okunev on 9.06.26.
+//
+
+import Testing
+
+struct GalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
